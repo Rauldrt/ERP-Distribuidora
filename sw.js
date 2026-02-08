1ca050240a84aa5f698098d1382e7737a2f1b57b
Rauldrt <102054424+Rauldrt@users.noreply.github.com>
2026-02-08 19:45:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-const CACHE_NAME = 'lacteos-v1'; const urlsToCache = [ './', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/icon-192-maskable.png', './icons/icon-512-maskable.png' ];
+const CACHE_NAME = 'lacteos-v1-safe';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
-self.addEventListener('install', event => { event.waitUntil( caches.open(CACHE_NAME) .then(cache => cache.addAll(urlsToCache)) ); });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cacheando archivos esenciales...');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.log('❌ Error cacheando archivos. Verifica que existan las imágenes:', err))
+  );
+});
 
-self.addEventListener('fetch', event => { event.respondWith( caches.match(event.request) .then(response => response || fetch(event.request)) ); });
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
